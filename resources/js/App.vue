ad104beba15b396c9b39822dce84084df07beb69
jasonkristanto <jasonkristanto4@gmail.com>
2020-12-15 23:05:03 +0700

    Initial laravel, vue, bootstrap, vuetify

---
+++ resources/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app>
+        <v-main>
+            Hello World
+            <v-btn
+                color="primary"
+                elevation="2"
+                medium
+            >This is button</v-btn>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
