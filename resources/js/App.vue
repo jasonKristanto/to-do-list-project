a68c94ebd59c55bb3fd7e158f89d77fa3b496c25
jasonkristanto <jasonkristanto4@gmail.com>
2021-01-15 23:12:51 +0700

    Center element on homepage

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,36 +2,38 @@
     <v-app>
         <v-main>
             <section>
-                <v-parallax src="/assets/homepage.jpg" light>
-                    <v-row align="center" justify="center">
-                        <v-col class="text-center" cols="12">
-                            <div class="brown lighten-2">
-                                <h1 class="display-1 font-weight-thin mb-4">
-                                    Todoful
-                                </h1>
-                                <h4 class="subheading">
-                                    Do what you have to do today, <br> so every day you live your life with fullest!
-                                </h4>
-                            </div>
-                        </v-col>
-                    </v-row>
-                    <v-row align="center" justify="center">
-                        <v-col cols="6">
-                            <login-component></login-component>
-                        </v-col>
-                        <v-col cols="6">
-                            <register-component></register-component>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="6">
-                            <v-btn block @click="getTodosTitle">GET TODOS</v-btn>
-                        </v-col>
-                        <v-col cols="6">
-                            <v-btn block @click="logout">LOGOUT</v-btn>
-                        </v-col>
-                    </v-row>
-                </v-parallax>
+                <div class="bg">
+                    <div class="main">
+                        <div class="custom-center" style="background-color: #9da3a3a8;">
+                            <v-row align="center" justify="center">
+                                <v-col class="text-center" cols="12">
+                                    <h1 class="display-1 font-weight-thin mb-4">
+                                        My To-Do Lists
+                                    </h1>
+                                    <h4 class="subheading">
+                                        Let's do all your to-do lists today to live life the fullest!
+                                    </h4>
+                                </v-col>
+                            </v-row>
+                            <v-row align="center" justify="center">
+                                <v-col cols="6">
+                                    <login-component></login-component>
+                                </v-col>
+                                <v-col cols="6">
+                                    <register-component></register-component>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="6">
+                                    <v-btn block @click="getTodosTitle">GET TODOS</v-btn>
+                                </v-col>
+                                <v-col cols="6">
+                                    <v-btn block @click="logout">LOGOUT</v-btn>
+                                </v-col>
+                            </v-row>
+                        </div>
+                    </div>
+                </div>
             </section>
         </v-main>
     </v-app>
@@ -92,5 +94,30 @@ export default {
 </script>
 
 <style scoped>
+.bg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: url('https://images.unsplash.com/photo-1530041859951-6cbea5e69b14?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80') no-repeat center center;
+    background-size: cover;
+}
 
+.main {
+    height: 100%;
+    position: relative;
+}
+
+.custom-center {
+    margin: 0;
+    position: absolute;
+    width: 75%;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+    padding: 20px;
+}
 </style>
--- routes/api/todos/todos.php
+++ routes/api/todos/todos.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Todos\TodosTitleController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
